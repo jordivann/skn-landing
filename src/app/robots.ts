758af7dff9d99c127ac991eb9cdd05394f3fdb51
Jordi <jordievannorden@gmil.com>
2026-01-13 18:39:29 -0300

    fix 1000

---
+++ src/app/robots.ts
@@ -0,0 +1,10 @@
+export const dynamic = "force-static";
+
+export default function robots() {
+  const base = process.env.NEXT_PUBLIC_SITE_URL ?? "http://localhost:3000";
+
+  return {
+    rules: [{ userAgent: "*", allow: "/" }],
+    sitemap: `${base}/sitemap.xml`,
+  };
+}
